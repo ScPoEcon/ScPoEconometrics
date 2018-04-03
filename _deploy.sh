75b2a267ee013fea6667a52b4d6810edc8908e5e
Florian Oswald <florian.oswald@gmail.com>
2018-04-03 14:14:29 +0200

    deploy

--- _deploy.sh
+++ _deploy.sh
@@ -5,12 +5,13 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "florian.oswald@gmail.com  "
+git config --global user.email "florian.oswald@gmail.com"
 git config --global user.name "Florian Oswald"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
+git status
 git commit -m"Update the book" || true
-git push -q origin gh-pages
+git push origin gh-pages
