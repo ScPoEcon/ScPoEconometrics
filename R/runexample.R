6e183f5ffbdaafcb298b5573c27d329edfe8c64a
vviers <vincent.viers@sciencespo.fr>
2018-09-04 16:32:55 +0200

    added aboutApp function

--- R/runexample.R
+++ R/runexample.R
@@ -60,6 +60,38 @@ launchApp <- function(appname) {
   shiny::runApp(appDir, display.mode = "normal")
 }
 
+#' aboutApp : get more information about an App !
+#'
+#' @param appname string of which app you want to get info about
+#' This function opens the description of any given app
+#'
+#'@export
+aboutApp <- function(appname){
+
+  # locate all the shiny app examples that exist
+  validExamples <- list.files(system.file("shinys", package = "ScPoEconometrics"))
+
+  validExamplesMsg <-
+    paste0(
+      "Valid apps are: '",
+      paste(validExamples, collapse = "', '"),
+      "'")
+
+  # if an invalid example is given, throw an error
+  if (missing(appname) || !nzchar(appname) ||
+      !appname %in% validExamples) {
+    stop(
+      'Please run `launchApp()` with a valid app as an argument.\n',
+      validExamplesMsg,
+      call. = FALSE)
+  }
+
+  # render and launch the about document
+  appDir = system.file("shinys", "datasaurus/", package = "ScPoEconometrics")
+  rmarkdown::render(paste0(appDir, "about.Rmd"))
+  browseURL(paste0(appDir, "about.html"))
+}
+
 gitbook <- function(){
   bookdown::render_book('index.Rmd', 'bookdown::gitbook')
 }
@@ -67,5 +99,3 @@ gitbook <- function(){
 getprompt <- function(){
   return("#OUT>")
 }
-
-
