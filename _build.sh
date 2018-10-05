f032f502c8dfb66da03c49e38bfcb3f21c726930
Florian Oswald <florian.oswald@gmail.com>
2018-10-05 17:53:09 +0200

    deploy slides with book

--- _build.sh
+++ _build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# build book(s)
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+
+# build slides greater than chapter 4
+for ((i=4; i<=12; i++)); do
+    if [ -d "_slides/chapter$i" ]; then
+        Rscript -e "rmarkdown::render('_slides/chapter$i/chapter$i.Rmd')"
+    fi
+done
--- _deploy.sh
+++ _deploy.sh
@@ -13,6 +13,12 @@ git config --global user.name "Florian Oswald"
 git clone -b gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
+
+for ((i=4; i<=12; i++)); do
+    if [ -d "../_slides/chapter$i" ]; then
+        cp -r ../_slides/chapter$i ./slides/chapter$i
+    fi
+done
 git add --all *
 git status
 git commit -m"Update the book" || true
