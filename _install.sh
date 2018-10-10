6d55d87dba9c797c1e4237ec62d0673bd22de434
florian oswald <florian.oswald@gmail.com>
2018-10-10 15:38:48 +0200

    adde more refs [skip ci]

--- _install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-Rscript -e 'install.packages(c("tidyverse", "reshape2","Ecdat","learnr","plot3D","mvtnorm","datasauRus"))'
-Rscript -e 'if (!all(c("tidyverse", "reshape2","Ecdat","learnr","plot3D","mvtnorm","datasauRus") %in% installed.packages())) { q(status = 1, save = "no")}'
