a259920a9d64a02ce503de18c6e188bc1062c9e4
florian oswald <florian.oswald@gmail.com>
2018-10-11 21:13:35 +0200

    add emo in build

--- _build.sh
+++ _build.sh
@@ -6,6 +6,7 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
 # build slides greater than chapter 4
+Rscript -e "library(devtools); install_github('hadley/emo')"
 for i in 4 5 6 7 8 9; do
     if [ -d "_slides/chapter$i" ]; then
         Rscript -e "rmarkdown::render('_slides/chapter$i/chapter$i.Rmd')"
