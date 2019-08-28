aa7f95b6ff49b7d3243e5ce1fa22f1bbf06ef0e2
florian oswald <florian.oswald@gmail.com>
2019-08-28 13:41:09 +0200

    removed slides from book. changed build and deploy to account for that. put slides into _archive

--- _build.sh
+++ _build.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
+set -e
+
 # build book(s)
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
-# build slides greater than chapter 4
-Rscript -e "library(devtools); install_github('hadley/emo')"
-for i in 4 5 6 7 8 9; do
-    if [ -d "_slides/chapter$i" ]; then
-        Rscript -e "rmarkdown::render('_slides/chapter$i/chapter$i.Rmd')"
-    fi
-done
