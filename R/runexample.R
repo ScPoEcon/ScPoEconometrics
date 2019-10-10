73eca477fff131ad2b9aac957870b610b9ac4ce0
Florian Oswald <florian.oswald@gmail.com>
2019-10-10 11:49:49 +0200

    finished. transition to modenrdive.

--- R/runexample.R
+++ R/runexample.R
@@ -114,6 +114,12 @@ pdfbook <- function(){
   bookdown::render_book('index.Rmd', 'bookdown::pdf_book')
 }
 
+pasta_maker <- function(){
+  pasta_jar <- tibble::tibble(id = 1:1980,color = sample(c(rep("Red",488),rep("Green",492),rep("White",1000)), size = 1980 ))
+  usethis::use_data(pasta_jar,overwrite = TRUE)
+  pasta_jar
+}
+
 getprompt <- function(){
   return("#OUT>")
 }
